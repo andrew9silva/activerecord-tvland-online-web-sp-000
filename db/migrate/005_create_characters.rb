930fa439398c583ed0d4fd66e5a286f102939a70
Andrew Silva <andrewsilva9@gmail.com>
2019-10-09 23:47:55 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -4,6 +4,7 @@ class CreateCharacter < ActiveRecord::Migration[5.1]
 
     create_table :characters do |x|
       x.string :first
+      x.string :last_name
       x.integer :actor_id
       x.integer :show_id
     end
