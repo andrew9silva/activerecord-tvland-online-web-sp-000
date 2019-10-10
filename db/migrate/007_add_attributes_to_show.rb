b3a13a2602d9390371e942e3ec395d6dd644d83e
Andrew Silva <andrewsilva9@gmail.com>
2019-10-10 00:58:27 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_attributes_to_show.rb
+++ db/migrate/007_add_attributes_to_show.rb
@@ -4,7 +4,6 @@ class AddAttributesToShow < ActiveRecord::Migration[5.1]
 
     add_column :shows, :day, :string
     add_column :shows, :season, :string
-    add_column :shows, :genre, :string
   end
 
 end
